9466beabd77a1c0ff5e6e1fd281f1ba0d397cbdf
pdg <pdg@ologantr.xyz>
2021-08-21 16:24:35 +0000

    Add initial implementation of QueryGeneratorOutput

---
+++ QueryGeneratorOutput.php
@@ -0,0 +1,15 @@
+<?php
+namespace Pleraque;
+
+class QueryGeneratorOutput implements QueryOutput
+{
+    public function getOutput(\PDOStatement $stmt)
+    {
+        return (function() use ($stmt)
+        {
+            while($row = $stmt->fetch(\PDO::FETCH_ASSOC))
+                yield $row;
+        })();
+    }
+}
+?>
