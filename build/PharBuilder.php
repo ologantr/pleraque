1ebd79992b5910d99c12b3adf2f957ce0107f93d
pdg <pdg@ologantr.xyz>
2021-12-22 09:46:58 +0000

    build: guard against PHP 8.0.0 or lower
    while here, use a separate stub file

--- build/PharBuilder.php
+++ build/PharBuilder.php
@@ -1,19 +1,6 @@
 <?php
 $fileName = "pleraque.phar";
 $phar = new Phar($fileName);
-
-$stub = <<<EOSTUB
-<?php
-spl_autoload_register(function(string \$class) : void {
-	require "phar://pleraque.phar/" . 
-		str_replace("\\\\", "/",
-			str_replace("Pleraque\\\\", "", \$class))
-			. ".php";
-});
-Phar::mapPhar("pleraque.phar");
-__HALT_COMPILER();
-EOSTUB;
-
 $phar->buildFromDirectory("../Pleraque");
-$phar->setStub($stub);
+$phar->setStub(file_get_contents("Stub.php"));
 ?>
