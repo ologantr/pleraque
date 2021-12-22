79a304a9e0bfd095766a100b8aa16823df4e2ebc
pdg <pdg@ologantr.xyz>
2021-12-22 16:37:13 +0000

    EmptyResponse: add setHeaders() function

--- Pleraque/EmptyResponse.php
+++ Pleraque/EmptyResponse.php
@@ -3,6 +3,11 @@ namespace Pleraque;
 
 class EmptyResponse extends Response
 {
+    public function setHeaders() : void
+    {
+        return;
+    }
+
     public function return() : void
     {
         return;
